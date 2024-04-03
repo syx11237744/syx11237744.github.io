5a72da0112d810fa090d7de6146194bb8b16ef00
syx <syx1123774474@163.com>
2024-04-03 13:47:22 +0800

    first

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');// 获取myImage元素的src属性（即图片的URL）
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+// 获取按钮和标题的引用
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName(); 
+}
